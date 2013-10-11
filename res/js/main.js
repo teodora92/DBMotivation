848ff56e22b44664cdf6bf9452d571a04bd7d46a
Teodora <teodora.petrisor92@gmail.com>
2013-10-11 18:18:53 +0200

    splash

--- res/js/main.js
+++ res/js/main.js
@@ -1,3 +1,6 @@
+/*
+	Data structure for our message
+*/
 function dataItem(id, user, text, date) {
 	this.id = id;
 	this.user = user;
@@ -7,17 +10,22 @@ function dataItem(id, user, text, date) {
 
 
 function loadData() {
+	/*
+		Send a GET request to own server
+		Due to same-origin policy of javascript (browser-side programming language) which applies to XMLHttpRequest methods,
+		jsonp is used to bypass restrictions of SOP without using cookies
+		Configure ajax request to expect a json object wrapped in a callback function named 'callback'
+	*/
 	$.ajax({
-			type: 'GET',
-			url: rootURL,
-			dataType: 'jsonp',
-			jsonp: 'callback',
-			crossDomain: true,
-			success: function(data) {
-				
+			type: 'GET',	// send a GET request to our server
+			url: rootURL,	
+			dataType: 'jsonp',	// expect a jsonp response
+			jsonpCallback: 'callback',	// expect response to be wrapped in a callback function named 'callback'
+			success: function(data) {	// if call was success, display the response data
+				responseData = data;
 				displayData(data);
 			},
-			error: function(xhr, textStatus, thrownError) {
+			error: function(xhr, textStatus, thrownError) {	// if there was an error, display the appropriate message
 				if(textStatus == "timeout") {
 					alert('Please connect to the internet');
 				}
@@ -25,33 +33,31 @@ function loadData() {
 					
 					if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
 		
-						navigator.notification.alert('Die App muss neu gestartet werden');
+						navigator.notification.alert('The app should be restarted');
 					}
 					else {
-						alert('Die App muss neu gestartet werden');
+						alert('The app should be restarted');
 					}
-					
 				}
 			},
-			complete: function(request, textStatus) { //for additional info
-				//alert("complete: " + request.responseText + " " + textStatus);
-				
-			},
-			timeout: 10000,
-			
-			jsonpCallback: 'callback'
+			timeout: 10000 // timeout after 10 seconds
 		});
-		
-		//displayData('data');
+
 }
 
 function displayData(data) {
 	
+	/*
+		Empty the list
+	*/
 	$('#thelist').empty();
 	
 	var i;
 	var item, id, user, text, date;
 	
+	/*
+		Parse each item in data array
+	*/
 	for(i=0; i < data.message.length; ++i) {
 		item = data.message[i];
 		id = item.id;
@@ -59,47 +65,95 @@ function displayData(data) {
 		text = item.text;
 		date = item.date;
 		
-		$('#thelist').append('<li class="listItem" id="'+id+'"><div class="user">'+user+'</div><div class="date">'+date+'</div><div class="text">'+text+'</div></li>');
+		/*
+			Add each item to the list dynamically
+		*/
+		$('#thelist').append('<li index="'+i+'" class="listItem" id="'+id+'"><div class="user">'+user+'</div><div class="date">'+date+'</div><div class="text">'+text+'</div></li>');
 	}
 	
 	var pullDownEl = document.getElementById('pullDown');
 	var pullDownOffset = pullDownEl.offsetHeight;
 	
-	var scroll = new iScroll('centerContent', {
+	/*
+		Configure iScroll variable to enable scrolling on list
+	*/
+	centerScroll = new iScroll('centerContent', {
 		useTransition: true,
 		topOffset: pullDownOffset,
 		vScrollbar: false,
 		onRefresh: function () {
 			if (pullDownEl.className.match('loading')) {
 				pullDownEl.className = '';
-				pullDownEl.querySelector('.pullDownLabel').textContent = 'Herunterziehen...';
+				pullDownEl.querySelector('.pullDownLabel').textContent = 'Pull down...';
 			} 
 		},
 		onScrollMove: function () {
 			if (this.y > 5 && !pullDownEl.className.match('flip')) {
 				pullDownEl.className = 'flip';
-				pullDownEl.querySelector('.pullDownLabel').textContent = 'Loslassen...';
+				pullDownEl.querySelector('.pullDownLabel').textContent = 'Release...';
 				this.minScrollY = 0;
 			} else if (this.y < 5 && pullDownEl.className.match('flip')) {
 				pullDownEl.className = '';
-				pullDownEl.querySelector('.pullDownLabel').textContent = 'Herunterziehen...';
+				pullDownEl.querySelector('.pullDownLabel').textContent = 'Pull down...';
 				this.minScrollY = -pullDownOffset;
 			}
 		},
 		onScrollEnd: function () {
 			if (pullDownEl.className.match('flip')) {
 				pullDownEl.className = 'loading';
-				pullDownEl.querySelector('.pullDownLabel').textContent = 'Ladend...';				
-				//pullDownAction();	// Execute custom function (ajax call?)
-				
+				pullDownEl.querySelector('.pullDownLabel').textContent = 'Loading...';
+				/*
+					On release load the data again
+				*/
 				loadData();
 			} 
 		}
 	});
 	
+	/*
+		Bind click event to each list item
+	*/
 	$('.listItem').click(function() {
-		scroll.refresh();
+		displayDetailPage(this.getAttribute('index'));
 	});
+}
+
+function displayDetailPage(index) {
+	var item = responseData.message[index];
+	var	id = item.id;
+	var	user = item.user;
+	var	text = item.text;
+	var	date = item.date;
+
+	/*
+		Empty detail page and fill it with new data
+	*/
+	$('#rightWrapper').empty();
+	$('#rightWrapper').append('<h1>'+user+'</h1>');
+	$('#rightWrapper').append('<div class="date pageRightElement">'+date+'</div><hr><br><br>');
+	$('#rightWrapper').append('<div class="text pageRightElement">'+text+'</div><br><br>');
+	
+	/*
+		Create new scroll variable
+		If needed, destroy old one to make space
+	*/
+	if(rightScroll != null) {
+		rightScroll.destroy();
+		rightScroll = null;
+	}
+	
+	
+	rightScroll = new iScroll('rightContent', {vScrollbar: false, hScrollbar: false, zoom: true, 
+	onZoomEnd: function() { 
+		currentScale = this.scale; // update current zooming scale
+	}
+	});
+	
+	/*
+		Make right page visible by changing class
+		Invisible class has margin-left: 100%, visible one has margin-left: 0
+	*/
+	document.getElementById('pageRight').setAttribute('class', 'pageRightVis');
 }
 
 /*
@@ -107,46 +161,24 @@ function displayData(data) {
 */
 function bindEvents() {
 
-	$('#pageCenter').swipe({
-		swipe: function(event, direction, distance, duration, fingerCount) {
-			if(direction == "left") {
-				document.getElementById('pageLeft').setAttribute('class', 'pageLeftInv');
-				document.getElementById('pageCenter').setAttribute('class', 'pageCenterVis');
-			}
-			
-			if(direction == "right") {
-				//document.getElementById('pageLeft').setAttribute('class', 'pageLeftVis');
-				//document.getElementById('pageCenter').setAttribute('class', 'pageCenterInv');
-			}
-		}
-	});
-	
-	
-	$('#pageLeft').swipe({
-		swipe: function(event, direction, distance, duration, fingerCount) {
-			if(direction == "left") {
-				document.getElementById('pageLeft').setAttribute('class', 'pageLeftInv');
-				document.getElementById('pageCenter').setAttribute('class', 'pageCenterVis');
-			}
-		}
-	});
-	
-	
+	/*
+		Configure touch events for right page navigation
+	*/
 	$('#pageRight').swipe({
 		swipe: function(event, direction, distance, duration, fingerCount) {
 			if(direction == "right") {
-				// here check current scale
+				/*
+					If zoomed in, disable swiping for navigation
+				*/
+				if(currentScale != 1.0) {
+					return;
+				}
 				document.getElementById('pageRight').setAttribute('class', 'pageRightInv');
 			}
 		}	
 	});
-	
-	menuScroll = new iScroll('menuContent', {
-		vScrollbar: false,
-		hScrollbar: false,
-		bounce: false
-	});
-	
+
+
 }
 
 /*
@@ -156,19 +188,25 @@ function bindEvents() {
 {
 
 	// universal variables
-	var menuScroll;
+	var centerScroll = null, rightScroll = null;
 	var rootURL = "http://internet-services.biz/dbmotivation/service/message";
-	// on load behavior
+	var responseData;
+	var currentScale = 1.0;
+	
+	
+	/*
+		After DOM content is loaded, load data asynchronously with ajax call
+	*/
 	document.addEventListener('DOMContentLoaded', function () { 
 		loadData();
 	}, false);
 	
-	// back button and menu button functions for Android
+	// back button and menu button functions for Android - recognized by phonegap
 	document.addEventListener("deviceready", function() {	
 		
 		//document.removeEventListener('backbutton', goLeft, false);
 		document.addEventListener("backButton", function() {
-			
+			device.exitApp();
 		}, false);
 		document.addEventListener("menubutton", function() {
 			
@@ -176,11 +214,16 @@ function bindEvents() {
 			
 	}, false);
 	
-	// resume app behavior
+	/*
+		On app resume, reload the data
+	*/
 	document.addEventListener("resume", function() {
-		
+		loadData
 	}, false);
 
+	/*
+		Bind events for navigation
+	*/
 	bindEvents();
 	
 }
